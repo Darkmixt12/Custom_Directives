f4948ea9d813271145661b4f1f7a817832408424
Steven Muñoz <darkmixt@gmail.com>
2023-10-20 18:01:40 -0600

    signals

---
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../interfaces/user-request.interface';
+
+@Component({
+  templateUrl: './user-info-page.component.html',
+  styleUrls: ['./user-info-page.component.css']
+})
+export class UserInfoPageComponent implements OnInit{
+  private usersService = inject(UsersService);
+  public userId = signal(1)
+
+  public currentUser = signal<User|undefined>(undefined)
+  public userWasFound = signal(true)
+
+  ngOnInit():void{
+    this.loadUser( this.userId( ))
+  }
+
+  loadUser(id: number):void{
+    if(id <= 0 ) return;
+
+    this.userId.set(id)
+      
+  }
+}
